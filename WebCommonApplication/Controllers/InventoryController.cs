f29c7fbe5d65d6cdaf2fbe09381e6dfcf473b837
javi ruiz <javi ruiz>
2016-12-03 10:02:58 +0100

    My first commit working in visual studio

---
+++ WebCommonApplication/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCommonApplication.Models;
+
+namespace WebCommonApplication.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Play with the title of the Inventory";
+            var car = new Car();
+            return View(car);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Car car)
+        {
+            return View(car);
+        }
+
+    }
+}
